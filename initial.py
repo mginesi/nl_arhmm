93e34ebba0217baf5d008e2c0fa3f280710cd5bd
M Ginesi <michele.ginesi@gmail.com>
2020-06-09 18:37:05 +0200

    added initial.py

---
+++ initial.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class Initial(object):
+
+    def __init__(self, n_modes=3, density=None):
+        self.n_modes = n_modes
+        if density is None:
+            density = np.random.rand(self.n_modes)
+        if not(np.shape(density)[0] == self.n_modes):
+            raise ValueError('initial.py: shape of initial density not compatible with number of modes!')
+        density = np.maximum(density, 0.0)
+        density /= np.sum(density)
+        self.density = density
+
+    def sample(self):
+        # Method to sample an initial state from the density function
+        cpdf = np.cumsum(self.density)
+        rv = np.random.rand()
+        return np.where(rv < cpdf)[-1][0]
+
+if __name__ == '__main__':
+    from initial import Initial
+    import matplotlib.pyplot as plt
+    in_d = Initial(5, np.ones(5))
+    sample_set = []
+    for _i in range(500):
+        sample_set.append(in_d.sample())
+    plt.hist(sample_set)
+    plt.show()
