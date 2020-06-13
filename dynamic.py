55f0732c30f277edd061c44dfca120936610f689
M Ginesi <michele.ginesi@gmail.com>
2020-06-13 15:19:46 +0200

    Dynamic.py: generalized learning method

--- dynamic.py
+++ dynamic.py
@@ -38,15 +38,18 @@ class Dynamic(object):
         err_set = output_set - pred_set
         return np.cov(np.transpose(err_set))
 
-    def learn_vector_field(self, input_set, output_set):
+    def learn_vector_field(self, input_set, output_set, weights=None):
         '''
         Compute the set of weights given the input and output set.
         '''
         n_data = len(input_set)
+        if weights is None:
+            weights = np.ones(n_data)
+        sqrt_weights = np.sqrt(np.asarray(weights))
         phi_mat = np.zeros([n_data, self.n_basis + 1])
         for _n in range(n_data):
-            phi_mat[_n] = self.compute_phi_vect(input_set[_n])
-        T = np.asarray(output_set)
+            phi_mat[_n] = sqrt_weights[_n] * self.compute_phi_vect(input_set[_n])
+        T = np.asarray(output_set) * np.reshape(sqrt_weights, [n_data, 1])
         self.weights = np.transpose(np.dot(np.linalg.pinv(phi_mat), T))
 
     def apply_vector_field(self, x):
@@ -64,31 +67,57 @@ for _rho in range(3):
         centers[8 * _rho + _theta, 1] = _rho * np.sin(_theta)
 dyn = Dynamic(2, centers, 0.2 * np.ones(24))
 
-def vf(x):
+def vf_stable(x):
     x1 = x[0]
     x2 = x[1]
     f1 = x1 ** 3 + x2 ** 2 * x1 - x1 - x2
     f2 = x2 ** 3 + x1 ** 2 * x2 + x1 - x2
     return np.array([f1, f2])
 
+def vf_omega_lim(x):
+    x1 = x[0]
+    x2 = x[1]
+    f1 = x1 ** 3 + x2 ** 2 * x1 - x1 - x2
+    f2 = x2 ** 3 + x1 ** 2 * x2 + x1 - x2
+    return - np.array([f1, f2])
+
 # Creation of the input and output set (Euler scheme used)
 n_sample = 500
 dt = 0.01
 input_set = []
 output_set = []
+
+# Stable data
 for _ in range(n_sample):
     _rho = np.random.rand()
     _theta = np.random.rand() * 2.0 * np.pi
     _in = _rho * np.array([np.cos(_theta), np.sin(_theta)])
-    _out = _in + dt * vf(_in)
+    _out = _in + dt * vf_stable(_in)
     input_set.append(_in)
     output_set.append(_out)
 
+# Omega limit data
+for _ in range(n_sample):
+    _rho = np.random.rand()
+    _theta = np.random.rand() * 2.0 * np.pi
+    _in = _rho * np.array([np.cos(_theta), np.sin(_theta)])
+    _out = _in + dt * vf_omega_lim(_in)
+    input_set.append(_in)
+    output_set.append(_out)
+
+# Three set of weights
+w_stable = np.ones(2*n_sample)
+w_stable[n_sample:] = 0.0
+w_omega_lim = np.ones(2*n_sample)
+w_omega_lim[:n_sample] = 0.0
+w_mix = np.ones(2*n_sample)
+
+# Stable
 # Learning of the vector field
-dyn.learn_vector_field(input_set, output_set)
+dyn.learn_vector_field(input_set, output_set, w_stable)
 
 # Simulation of the learned v.f.
-rho = np.random.rand()
+rho = np.random.rand() * 0.5 + 0.5
 theta = np.random.rand() * 2.0 * np.pi
 _x = rho * np.array([np.cos(theta), np.sin(theta)])
 _x_t = rho * np.array([np.cos(theta), np.sin(theta)])
@@ -96,7 +125,7 @@ x = [_x]
 x_true = [_x_t]
 for t in range(1000):
     _x = dyn.apply_vector_field(_x)
-    _x_t = _x_t + dt * vf(_x_t)
+    _x_t = _x_t + dt * vf_stable(_x_t)
     x.append(_x)
     x_true.append(_x_t)
 x = np.asarray(x)
@@ -109,4 +138,58 @@ plt.plot(x[-1][0], x[-1][1], 'xk', label='xT')
 plt.legend(loc='best')
 plt.xlim([-1, 1])
 plt.ylim([-1, 1])
+plt.title('Only stable')
+
+# Omega lim
+# Learning of the vector field
+dyn.learn_vector_field(input_set, output_set, w_omega_lim)
+
+# Simulation of the learned v.f.
+rho = np.random.rand() * 0.5 + 0.5
+theta = np.random.rand() * 2.0 * np.pi
+_x = rho * np.array([np.cos(theta), np.sin(theta)])
+_x_t = rho * np.array([np.cos(theta), np.sin(theta)])
+x = [_x]
+x_true = [_x_t]
+for t in range(1000):
+    _x = dyn.apply_vector_field(_x)
+    _x_t = _x_t + dt * vf_omega_lim(_x_t)
+    x.append(_x)
+    x_true.append(_x_t)
+x = np.asarray(x)
+x_true = np.asarray(x_true)
+plt.figure()
+plt.plot(x[:, 0], x[:, 1], 'r', label='learned')
+plt.plot(x_true[:, 0], x_true[:, 1], '--b', label='true')
+plt.plot(x[0, 0], x[0, 1], 'ok', label='x0')
+plt.plot(x[-1][0], x[-1][1], 'xk', label='xT')
+plt.legend(loc='best')
+plt.xlim([-1, 1])
+plt.ylim([-1, 1])
+plt.title('Only omega lim')
+
+# Mixed
+# Learning of the vector field
+dyn.learn_vector_field(input_set, output_set, w_mix)
+
+# Simulation of the learned v.f.
+rho = np.random.rand() * 0.5 + 0.5
+theta = np.random.rand() * 2.0 * np.pi
+_x = rho * np.array([np.cos(theta), np.sin(theta)])
+_x_t = rho * np.array([np.cos(theta), np.sin(theta)])
+x = [_x]
+for t in range(1000):
+    _x = dyn.apply_vector_field(_x)
+    _x_t = _x_t + dt * vf_omega_lim(_x_t)
+    x.append(_x)
+x = np.asarray(x)
+plt.figure()
+plt.plot(x[:, 0], x[:, 1], 'r', label='learned')
+plt.plot(x[0, 0], x[0, 1], 'ok', label='x0')
+plt.plot(x[-1][0], x[-1][1], 'xk', label='xT')
+plt.legend(loc='best')
+plt.xlim([-1, 1])
+plt.ylim([-1, 1])
+plt.title('Mixed')
+
 plt.show()
