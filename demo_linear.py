df1e5b7b8829462a3a168083a403eff4608f480f
M Ginesi <michele.ginesi@gmail.com>
2020-07-03 10:56:54 +0200

    improvements to demo

--- demo_linear.py
+++ demo_linear.py
@@ -2,28 +2,34 @@ import numpy as np
 from nl_arhmm import Linear_ARHMM
 import matplotlib.pyplot as plt
 
-# True trajectory is a square
-
-interval = np.linspace(0, 1, 50)
-bottom = np.array([interval, np.zeros(50)]).transpose()
-right = np.array([np.ones(50), interval]).transpose()
-top = np.array([np.flip(interval), np.ones(50)]).transpose()
-left = np.array([np.zeros(50), np.flip(interval)]).transpose()
-
-data = np.concatenate([bottom, right, top, left])
-
 # ARHMM
-dyn_mtrxs = [10 * np.random.rand(2,3), 10 * np.random.rand(2,3), 10 * np.random.rand(2,3), 10 * np.random.rand(2,3)]
+
+# True model
+dyn_mtrxs = [0.5 * np.random.rand(2,3), 0.5 * np.random.rand(2,3)]
 sigmas = []
 for _n in range(4):
-    rm = np.random.rand(2, 2)
-    sigmas.append(np.dot(rm, rm.transpose()))
-model = Linear_ARHMM(2, 4, dyn_mtrxs, sigmas)
+    rm = np.eye(2)
+    sigmas.append(rm)
+model_true = Linear_ARHMM(2, 2, dyn_mtrxs, sigmas)
 
-model.em_algorithm(data)
+[x_true, mode_true] = model_true.simulate(np.random.rand(2))
+mode_infered_true = model_true.viterbi(x_true)
 
-mode_inferred = model.viterbi(data)
+# Test model
+dyn_mtrxs = [0.5 * np.random.rand(2,3), 0.5 * np.random.rand(2,3)]
+sigmas = []
+for _n in range(4):
+    rm = np.eye(2)
+    sigmas.append(rm)
+model = Linear_ARHMM(2, 2, dyn_mtrxs, sigmas)
+model.em_algorithm(x_true)
+mode_infered = model.viterbi(x_true)
 
 plt.figure()
-plt.imshow(np.array([mode_inferred]), aspect='auto')
+plt.subplot(311)
+plt.imshow(np.array([mode_true]), aspect='auto')
+plt.subplot(312)
+plt.imshow(np.array([mode_infered_true]), aspect='auto')
+plt.subplot(313)
+plt.imshow(np.array([mode_infered]), aspect='auto')
 plt.show()
