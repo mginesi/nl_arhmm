4dfaa689d61807fb1bce4e75db600ede498081de
M Ginesi <michele.ginesi@gmail.com>
2020-09-23 14:56:08 +0200

    normal prob: added flags to use inverse covariance

--- nl_arhmm/utils.py
+++ nl_arhmm/utils.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-def normal_prob(y, mu, sigma):
+def normal_prob(y, mu, sigma, inv=False):
     '''
     Return the normal pdf with mean mu and covariance matrix sigma evaluated in y.
+    If the inverse covariance matrix is passed, set inv=True.
     '''
     if np.isscalar(mu) and np.isscalar(sigma) and np.isscalar(y):
-        return 0.3989422804014327 / np.sqrt(sigma) * np.exp(-0.5 * (y - mu) * (y - mu) / sigma)
+        if not inv:
+            return 0.3989422804014327 / np.sqrt(sigma) * np.exp(-0.5 * (y - mu) * (y - mu) / sigma)
+        else:
+            return 0.3989422804014327 * np.sqrt(sigma) * np.exp(-0.5 * (y - mu) * (y - mu) * sigma)
     else:
         # Check number of dimensions
         if not (np.ndim(mu) == 1 and np.ndim(sigma) == 2 and np.ndim(y) == 1):
@@ -13,17 +17,25 @@ def normal_prob(y, mu, sigma):
         k = np.shape(mu)[0]
         if not (np.shape(sigma) == (k, k)):
             raise ValueError('sigma must be a square matrix with number or rows equals to mu and y')
-        sigma_inv = np.linalg.pinv(sigma)
-        det_sigma = np.linalg.det(sigma)
+        if not inv:
+            sigma_inv = np.linalg.pinv(sigma)
+            det_sigma = np.linalg.det(sigma)
+        else:
+            sigma_inv = sigma
+            det_sigma = 1.0 / np.linalg.det(sigma_inv)
         const = (0.3989422804014327 ** k) / np.sqrt(det_sigma)
         return const * np.exp(-0.5 * np.dot(y - mu, np.dot(sigma_inv, y - mu)))
 
-def log_normal_prob(y, mu, sigma):
+def log_normal_prob(y, mu, sigma, inv=False):
     '''
     Return the normal pdf with mean mu and covariance matrix sigma evaluated in y.
+    If the inverse covariance matrix is passed, set inv=True.
     '''
     if np.isscalar(mu) and np.isscalar(sigma) and np.isscalar(y):
-        return -0.9189385332046727 - 0.5 * np.log(sigma) - 0.5 * (y - mu) * (y - mu) / sigma
+        if not inv:
+            return -0.9189385332046727 - 0.5 * np.log(sigma) - 0.5 * (y - mu) * (y - mu) / sigma
+        else:
+            return -0.9189385332046727 + 0.5 * np.log(sigma) - 0.5 * (y - mu) * (y - mu) * sigma
     else:
         # Check number of dimensions
         if not (np.ndim(mu) == 1 and np.ndim(sigma) == 2 and np.ndim(y) == 1):
@@ -31,8 +43,12 @@ def log_normal_prob(y, mu, sigma):
         k = np.shape(mu)[0]
         if not (np.shape(sigma) == (k, k)):
             raise ValueError('sigma must be a square matrix with number or rows equals to mu and y')
-        sigma_inv = np.linalg.pinv(sigma)
-        det_sigma = np.linalg.det(sigma)
+        if not inv:
+            sigma_inv = np.linalg.pinv(sigma)
+            det_sigma = np.linalg.det(sigma)
+        else:
+            sigma_inv = sigma
+            det_sigma = 1.0 / np.linalg.det(sigma)
         return - k * 0.9189385332046727 - 0.5 * np.log(det_sigma) - 0.5 * \
             np.dot(y - mu, np.dot(sigma_inv, y - mu))
 
